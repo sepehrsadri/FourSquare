3db0854357ce58dc4c4a1fd2ac71b80e8b8b3ec5
Sepehr Sadri <sepehrsadri@gmail.com>
2020-05-31 03:22:13 +0430

    Add base test requirements

---
+++ app/src/test/java/com/sadri/foursquare/general/InstantExecutorExtension.kt
@@ -0,0 +1,27 @@
+package com.sadri.foursquare.general
+
+import androidx.arch.core.executor.ArchTaskExecutor
+import androidx.arch.core.executor.TaskExecutor
+import org.junit.jupiter.api.extension.AfterEachCallback
+import org.junit.jupiter.api.extension.BeforeEachCallback
+import org.junit.jupiter.api.extension.ExtensionContext
+
+/**
+ * Created by Sepehr Sadri on 5/31/2020.
+ * sepehrsadri@gmail.com
+ * Tehran, Iran.
+ * Copyright © 2020 by Sepehr Sadri. All rights reserved.
+ */
+internal class InstantExecutorExtension : BeforeEachCallback, AfterEachCallback {
+    override fun beforeEach(context: ExtensionContext?) {
+        ArchTaskExecutor.getInstance().setDelegate(object : TaskExecutor() {
+            override fun executeOnDiskIO(runnable: Runnable) = runnable.run()
+            override fun postToMainThread(runnable: Runnable) = runnable.run()
+            override fun isMainThread(): Boolean = true
+        })
+    }
+
+    override fun afterEach(context: ExtensionContext?) {
+        ArchTaskExecutor.getInstance().setDelegate(null)
+    }
+}
