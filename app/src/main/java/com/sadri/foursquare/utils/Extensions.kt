60c929ba474e6533ac812ba0d49f9adb10f03765
Sepehr Sadri <sepehrsadri@gmail.com>
2020-05-31 14:25:11 +0430

    Add global extensions utils

---
+++ app/src/main/java/com/sadri/foursquare/utils/Extensions.kt
@@ -0,0 +1,23 @@
+package com.sadri.foursquare.utils
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+
+/**
+ * Created by Sepehr Sadri on 5/31/2020.
+ * sepehrsadri@gmail.com
+ * Tehran, Iran.
+ * Copyright © 2020 by Sepehr Sadri. All rights reserved.
+ */
+fun Boolean?.isFalseOrNull(): Boolean {
+    return when {
+        this == null -> true
+        else -> !this
+    }
+}
+
+fun <T : Any?> LiveData<T>.observeWithInitUpdate(owner: LifecycleOwner, observer: Observer<T>) {
+    this.observe(owner, observer)
+    observer.onChanged(value)
+}
