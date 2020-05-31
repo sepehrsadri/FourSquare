7d18986f9e3a15439424f3e683af439754db0d40
Sepehr Sadri <sepehrsadri@gmail.com>
2020-05-31 10:46:20 +0000

    Add connection state monitor

---
+++ app/src/main/java/com/sadri/foursquare/utils/network/NetworkUtils.kt
@@ -0,0 +1,42 @@
+package com.sadri.foursquare.utils.network
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+
+/**
+ * Created by Sepehr Sadri on 5/31/2020.
+ * sepehrsadri@gmail.com
+ * Tehran, Iran.
+ * Copyright © 2020 by Sepehr Sadri. All rights reserved.
+ */
+fun Context.isNetworkAvailable(): Boolean {
+    var result = false
+    val connectivityManager =
+        this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        val networkCapabilities = connectivityManager.activeNetwork ?: return false
+        val actNw =
+            connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
+        result = when {
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            else -> false
+        }
+    } else {
+        @Suppress("DEPRECATION")
+        connectivityManager.run {
+            connectivityManager.activeNetworkInfo?.run {
+                result = when (type) {
+                    ConnectivityManager.TYPE_WIFI -> true
+                    ConnectivityManager.TYPE_MOBILE -> true
+                    ConnectivityManager.TYPE_ETHERNET -> true
+                    else -> false
+                }
+            }
+        }
+    }
+    return result
+}
