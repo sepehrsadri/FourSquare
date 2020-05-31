b0a6d07bd8f8d3b3cd41737ba2d2f20d75e51cac
Sepehr Sadri <sepehrsadri@gmail.com>
2020-05-31 20:24:26 +0430

    Add dashboard fragment and view model

---
+++ app/src/main/java/com/sadri/foursquare/ui/screens/dashboard/fragments/dashboard/DashboardViewModel.kt
@@ -0,0 +1,12 @@
+package com.sadri.foursquare.ui.screens.dashboard.fragments.dashboard
+
+import com.sadri.foursquare.ui.navigation.NavigationViewModel
+import javax.inject.Inject
+
+/**
+ * Created by Sepehr Sadri on 5/31/2020.
+ * sepehrsadri@gmail.com
+ * Tehran, Iran.
+ * Copyright © 2020 by Sepehr Sadri. All rights reserved.
+ */
+class DashboardViewModel @Inject constructor() : NavigationViewModel()
