ad8ca5a9d8617891dd1af16c2ad394a226b29847
Sepehr Sadri <sepehrsadri@gmail.com>
2020-06-05 17:29:37 +0000

    Add words extractor utility component
    + Separate only english words of venue name

---
+++ app/src/main/java/com/sadri/foursquare/ui/screens/dashboard/fragments/dashboard/DashboardListAdapter.kt
@@ -0,0 +1,78 @@
+package com.sadri.foursquare.ui.screens.dashboard.fragments.dashboard
+
+import android.view.View
+import androidx.recyclerview.widget.DiffUtil
+import com.google.android.material.textview.MaterialTextView
+import com.sadri.foursquare.R
+import com.sadri.foursquare.models.venue.Venue
+import com.sadri.foursquare.ui.adapter.BindingViewHolder
+import com.sadri.foursquare.ui.adapter.GenericListAdapter
+import com.sadri.foursquare.ui.utils.setTextFromResourcesWithArg
+import com.sadri.foursquare.utils.extractEnDigits
+
+/**
+ * Created by Sepehr Sadri on 6/3/2020.
+ * sepehrsadri@gmail.com
+ * Tehran, Iran.
+ * Copyright © 2020 by Sepehr Sadri. All rights reserved.
+ */
+class DashboardListAdapter(
+    private val dashboardViewModel: DashboardViewModel
+) : GenericListAdapter<Venue, BindingViewHolder<Venue>>(VenueDiffCallback()) {
+
+    override fun getLayoutId(position: Int, obj: Venue) =
+        R.layout.list_item_venue
+
+    override fun getViewHolder(view: View, viewType: Int): BindingViewHolder<Venue> {
+        return DashboardListViewHolder(
+            view,
+            dashboardViewModel
+        )
+    }
+
+    private class DashboardListViewHolder(
+        private val item: View,
+        private val dashboardViewModel: DashboardViewModel
+    ) : BindingViewHolder<Venue>(item) {
+        private val txtVenueName = item.findViewById<MaterialTextView>(
+            R.id.venueNameTv
+        )
+
+        private val txtVenueCategory = item.findViewById<MaterialTextView>(
+            R.id.venueCategoryTv
+        )
+
+        private val txtVenueDistance = item.findViewById<MaterialTextView>(
+            R.id.venueDistanceTv
+        )
+
+        private lateinit var data: Venue
+
+        private val clickListener = View.OnClickListener {
+            dashboardViewModel.onVenueClick(data)
+        }
+
+        override fun bind(data: Venue) {
+            this.data = data
+
+            txtVenueName.text = data.name.extractEnDigits()
+            txtVenueCategory.text = data.categories.firstOrNull()?.name
+            txtVenueDistance.setTextFromResourcesWithArg(
+                R.string.distance_place_holder,
+                data.location.distance.toString()
+            )
+
+            item.setOnClickListener(clickListener)
+        }
+    }
+
+    private class VenueDiffCallback : DiffUtil.ItemCallback<Venue>() {
+        override fun areItemsTheSame(oldItem: Venue, newItem: Venue): Boolean {
+            return newItem.id == oldItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Venue, newItem: Venue): Boolean {
+            return newItem == oldItem
+        }
+    }
+}
--- app/src/main/java/com/sadri/foursquare/ui/screens/dashboard/fragments/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/sadri/foursquare/ui/screens/dashboard/fragments/dashboard/DashboardViewModel.kt
@@ -1,9 +1,20 @@
 package com.sadri.foursquare.ui.screens.dashboard.fragments.dashboard
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
+import androidx.lifecycle.viewModelScope
+import com.sadri.foursquare.components.location.LocationProvider
 import com.sadri.foursquare.components.permission.PermissionProvider
+import com.sadri.foursquare.data.repositories.explore.EXPLORE_DATA_OFFSET
+import com.sadri.foursquare.data.repositories.explore.ExploreDataSingleSourceOfTruth
+import com.sadri.foursquare.data.repositories.explore.ExploreResult
+import com.sadri.foursquare.data.utils.Result
+import com.sadri.foursquare.models.venue.Venue
 import com.sadri.foursquare.ui.navigation.NavigationViewModel
 import com.sadri.foursquare.utils.gps.GpsStateMonitor
+import com.sadri.foursquare.utils.live_data.SingleLiveEvent
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 /**
@@ -14,8 +25,59 @@ import javax.inject.Inject
  */
 class DashboardViewModel @Inject constructor(
     gpsStateMonitor: GpsStateMonitor,
-    private val permissionProvider: PermissionProvider
+    private val permissionProvider: PermissionProvider,
+    private val exploreDataSingleSourceOfTruth: ExploreDataSingleSourceOfTruth,
+    locationProvider: LocationProvider
 ) : NavigationViewModel() {
+    val toast = SingleLiveEvent<String>()
+
+    private val _venues = MutableLiveData<List<Venue>>()
+    val venues: LiveData<List<Venue>>
+        get() = _venues
+
+    private var page: Int = 0
+
+    private val venuesList: MutableList<Venue> = ArrayList()
+
+    private var isLoading = false
+
+    private val exploreObserver =
+        Observer<Result<ExploreResult>> {
+            when (it) {
+                is Result.Loading -> {
+                    fullscreenLoading.value = true
+                }
+                is Result.Success -> {
+                    updateVenuesList(it)
+                    fullscreenLoading.value = false
+                    isLoading = false
+                }
+                is Result.Error -> {
+                    fullscreenLoading.value = false
+                    toast.value = it.error.message
+                    isLoading = false
+                }
+            }
+        }
+
+    private fun updateVenuesList(it: Result.Success<ExploreResult>) {
+        val data = it.data
+        val venues = data.result
+        val offset = data.offset
+        val reliable = data.reliable
+
+        if (reliable && venuesList.size >= offset + EXPLORE_DATA_OFFSET) {
+
+            for ((index, i) in
+            (offset until offset + EXPLORE_DATA_OFFSET).withIndex()) {
+                venuesList[i] = venues[index]
+            }
+        } else {
+            venuesList.addAll(venues)
+        }
+
+        _venues.value = ArrayList(venuesList.toList())
+    }
 
     init {
         observeWithInitUpdate(
@@ -26,5 +88,51 @@ class DashboardViewModel @Inject constructor(
                 }
             }
         )
+        observe(
+            locationProvider.locationChange,
+            Observer {
+                venuesList.clear()
+                page = 0
+                fetchVenues()
+            }
+        )
+    }
+
+    fun onVenueClick(venue: Venue) {
+        toast.value = venue.name
+    }
+
+    fun onScroll() {
+        page++
+        fetchVenues()
+    }
+
+    private fun fetchVenues() {
+        if (isLoading) {
+            return
+        }
+        isLoading = true
+
+        observe(
+            exploreDataSingleSourceOfTruth.fetchExplores(
+                page * EXPLORE_DATA_OFFSET
+            ),
+            exploreObserver
+        )
+    }
+
+    fun initFetch() {
+        if (venuesList.isEmpty()) {
+            fetchVenues()
+        }
+    }
+
+    fun refreshExplores() {
+        viewModelScope.launch {
+            exploreDataSingleSourceOfTruth.explorePersistentDataSource.clear()
+            venuesList.clear()
+            page = 0
+            fetchVenues()
+        }
     }
 }
